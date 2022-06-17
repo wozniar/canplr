ee3c1990a8afb8c70ac3bf34eb2470c9448dd0a1
wozniar <42768258+wozniar@users.noreply.github.com>
2022-06-17 22:01:53 +0200

    Add wrapper functions to read_data.R

--- R/read_data.R
+++ R/read_data.R
@@ -1,6 +1,36 @@
-read_data <- function() {
-  files <- list.files("data/", pattern = "csv", full.names = TRUE)
-  data <- map(files, read_csv)
-  names(data) <- str_remove(list.files("data/", pattern = "csv"), ".csv")
-  return(data)
+read_data <- function(data) {
+  files <- list.files("data/", pattern = data, full.names = TRUE)
+  list <- map(files, read_csv)
+  names(list) <- str_extract(files, "\\d+")
+  return(list)
+}
+
+read_player_by_game <- function() {
+  read_data("CPLPlayerByGame") |> 
+    bind_rows(.id = "season") |> 
+    mutate(season = as.numeric(season)) |> 
+    distinct_cols()
+}
+
+read_player_totals <- function() {
+  read_data("CPLPlayerTotals") |> 
+    bind_rows(.id = "season") |> 
+    mutate(season = as.numeric(season)) |> 
+    distinct_cols()
+}
+
+read_team_by_game <- function() {
+  read_data("CPLTeamByGame") |> 
+    map(~ .x |> mutate(xGPerShot = as.numeric(xGPerShot))) |>
+    map(~ .x |> mutate(Date = as.Date(Date, tryFormats = c("%Y-%m-%d", "%m/%d/%Y")))) |>
+    bind_rows(.id = "season") |> 
+    mutate(season = as.numeric(season)) |> 
+    distinct_cols()
+}
+
+read_team_totals <- function() {
+  read_data("CPLTeamTotals") |> 
+    bind_rows(.id = "season") |>
+    mutate(season = as.numeric(season)) |> 
+    distinct_cols()
 }
